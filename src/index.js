6e1d8b80fb4306fd37325056110875410390fa74
Sreeshks <sreeshksureshh@gmail.com>
2025-05-29 16:05:04 +0530

    cors policy added

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
