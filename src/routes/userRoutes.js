232b8a265b46ad309c9ecf0830ba2858ef14682f
Sreeshks <sreeshksureshh@gmail.com>
2025-05-30 15:43:51 +0530

    updated isssue

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-// const auth = require('../middleware/auth');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
@@ -9,9 +8,5 @@ router.get('/profile/:email', userController.getProfile);
 router.put('/profile/:email', userController.updateProfile);
 router.post('/forgot-password', userController.forgotPassword);
 router.post('/reset-password', userController.resetPassword);
-router.get('/all', userController.getAllUsers);
-
-// Get all turfs
-router.get('/turfs', userController.getAllTurfs);
 
 module.exports = router; 
