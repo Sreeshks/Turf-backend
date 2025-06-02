93f32a0db9134ebd0fd7b4a343109d90b5c7c726
Sreeshks <sreeshksureshh@gmail.com>
2025-06-02 17:02:40 +0530

    route udpated

--- src/routes/turfOwnerRoutes.js
+++ src/routes/turfOwnerRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const turfOwnerController = require('../controllers/turfOwnerController');
 
-router.post('/register', turfOwnerController.register);
+router.post('/register',upload.single('image'), turfOwnerController.register);
 router.post('/login', turfOwnerController.login);
 router.get('/profile/:email', turfOwnerController.getProfile);
 router.get('/profile/id/:turfId', turfOwnerController.getProfileById);
