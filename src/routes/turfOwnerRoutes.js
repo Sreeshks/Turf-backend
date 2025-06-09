75456905669fbc30ec6730f2109dbc4beed36098
Sreeshks <sreeshksureshh@gmail.com>
2025-06-09 12:38:33 +0530

    route issue solved

--- src/routes/turfOwnerRoutes.js
+++ src/routes/turfOwnerRoutes.js
@@ -2,13 +2,6 @@ const express = require('express');
 const router = express.Router();
 const turfController = require('../controllers/turfOwnerController');
 const { upload } = require('../middlewares/multer');
-
-router.post('/register', upload.single('image'), turfController.register);
-router.post('/login', turfController.login);
-router.get('/profile/:email', turfController.getProfile);
-router.get('/all', turfController.getAllTurfs);
-router.post('/forgot-password', turfController.forgotPassword);
-router.post('/reset-password', turfController.resetPassword);
-router.post('/addturf/:email', upload.single('image'), turfController.addturf);
+router.post('/addturf', upload.single('image'), turfController.addturf);
 router.get('/turfs/:email', turfController.getOwnerTurfs);
 module.exports = router;
