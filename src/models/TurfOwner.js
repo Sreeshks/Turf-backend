03559159b28ba370da924cac8f96a6c5f9c70085
Sreeshks <sreeshksureshh@gmail.com>
2025-06-09 10:05:45 +0530

    issue resolved

--- src/models/TurfOwner.js
+++ src/models/TurfOwner.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose');
-const { v4: uuidv4 } = require('uuid');
 
 const turfOwnerSchema = new mongoose.Schema({
-  turfId: { type: String, default: uuidv4, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
-  turfLocation: { type: String, required: true },
-  sports: [{ type: String, enum: ['Football', 'Cricket', 'Tennis', 'Badminton'], required: true }],
-  resetPasswordCode: { type: String },
-  resetPasswordExpires: { type: Date },
-  image: { type: String }
+email: { type: String, required: true, unique: true },
+password: { type: String, required: true },
+name: { type: String, required: true },
+turfLocation: { type: String, required: true },
+sports: [{ type: String, enum: ['Football', 'Cricket', 'Tennis', 'Badminton'], required: true }],
+resetPasswordCode: { type: String },
+resetPasswordExpires: { type: Date },
+image: { type: String }
 });
 
 module.exports = mongoose.model('TurfOwner', turfOwnerSchema);
--- src/routes/turfOwnerRoutes.js
+++ src/routes/turfOwnerRoutes.js
@@ -11,6 +11,4 @@ router.put('/profile/:email', turfController.updateProfile);
 router.get('/all', turfController.getAllTurfs);
 router.post('/forgot-password', turfController.forgotPassword);
 router.post('/reset-password', turfController.resetPassword);
-router.post('/addturf', upload.single('image'), turfController.addTurf);
-
 module.exports = router;
