ead295e08187f29dadaaeacec10e53dfb896d45c
Sreeshks <sreeshksureshh@gmail.com>
2025-06-16 16:59:09 +0530

    refactor userRoutes to delegate booking functionality to bookingController

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const bookingController = require('../controllers/bookingController');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
@@ -8,6 +9,6 @@ router.get('/profile/:email', userController.getProfile);
 router.put('/profile/:email', userController.updateProfile);
 router.post('/forgot-password', userController.forgotPassword);
 router.post('/reset-password', userController.resetPassword);
-router.post('/booking', userController.booking);
+router.post('/booking', bookingController.booking);
 
 module.exports = router; 
